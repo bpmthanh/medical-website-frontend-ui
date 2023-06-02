2712b7ede8ec8801cf1cdff1c294ad2511a9315e
Thanh Bui <ltvphuoctien@gmail.com>
2023-06-02 09:40:37 +0700

    feat:#38

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,33 +1,75 @@
-import React, { Component } from 'react';
-import { FormattedMessage } from 'react-intl';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { FormattedMessage } from "react-intl";
+import { connect } from "react-redux";
+import "./userManage.scss";
+import { getAllUsers } from "../../services/userService";
 class UserManage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      arrUsers: [],
+    };
+  }
 
-    state = {
-
+  async componentDidMount() {
+    try {
+      let response = await getAllUsers("All");
+      if (response && response.errCode === 0) {
+        this.setState({
+          arrUsers: response.users,
+        });
+      }
+    } catch (error) {
+      console.error(error);
     }
+  }
 
-    componentDidMount() {
-
-    }
-
-
-    render() {
-        return (
-            <div className="text-center">Manage users</div>
-        );
-    }
-
+  render() {
+    return (
+      <div className="uses-container">
+        <div className="title text-center">Manage user</div>
+        <div className="users-table container">
+          <table id="customers">
+            <tr>
+              <th>Email</th>
+              <th>First name</th>
+              <th>Last name</th>
+              <th>Address</th>
+              <th>Phone number</th>
+              <th>Action</th>
+            </tr>
+            {this.state.arrUsers.map((element) => {
+              return (
+                <tr key={element.id}>
+                  <td>{element.email}</td>
+                  <td>{element.firstName}</td>
+                  <td>{element.lastName}</td>
+                  <td>{element.address}</td>
+                  <td>{element.phoneNumber}</td>
+                  <td>
+                    <button className="bg-success" type="button">
+                      <a href="/edit-crud?id=<%= dataTable[i].id%>">Edit</a>
+                    </button>
+                    <button className="bg-danger" style={{"marginLeft": '20px'}} type="button">
+                      <a href="/delete-crud?id=<%= dataTable[i].id%>">Delete</a>
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = state => {
-    return {
-    };
+const mapStateToProps = (state) => {
+  return {};
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-    };
+const mapDispatchToProps = (dispatch) => {
+  return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserManage);
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,5 +1,9 @@
 import axios from "../axios";
 
 export const handleLoginAPI = (email, password) => {
-  return axios.post('/api/login',{email,password});
+  return axios.post("/api/login", { email, password });
+};
+
+export const getAllUsers = (inputId) => {
+  return axios.get(`/api/get-all-users?id=${inputId}`);
 };
