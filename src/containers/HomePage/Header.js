d3e1afedf1f26a58a9c54168c1e866ee0b5b4565
Thanh Bui <ltvphuoctien@gmail.com>
2023-06-07 10:13:29 +0700

    feat:#44

--- src/containers/HomePage/Header.js
+++ src/containers/HomePage/Header.js
@@ -2,18 +2,53 @@ import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import "./Header.scss";
+import "../../styles/global-class.scss";
+
 
 class Header extends Component {
   render() {
     return (
-      <div className="home-header-container">
+      <div className="container-layout home-header-container">
         <div className="home-header-content">
           <div className="home-header-content__left-content">
-            <i class="fa-solid fa-bars"></i>
+            <i className="fa-solid fa-bars"></i>
             <div className="home-header-content__left-content--menu-icon"></div>
           </div>
-          <div className="home-header-content__center-content"></div>
-          <div className="home-header-content__right-content"></div>
+          <div className="home-header-content__center-content">
+            <div className="menu-list">
+              <a href="#">
+                Chuyên khoa
+                <span>Tìm bác sĩ theo chuyên khoa</span>
+              </a>
+            </div>
+            <div className="menu-list">
+              <a href="#">
+                Cơ sở y tế
+                <span>Chọn bệnh viện phòng khám</span>
+              </a>
+            </div>
+            <div className="menu-list">
+              <a href="#">
+                Bác sĩ
+                <span>Chọn bác sĩ giỏi</span>
+              </a>
+            </div>
+            <div className="menu-list">
+              <a href="#">
+                Gói khám
+                <span>Khám sức khỏe tổng quát</span>
+              </a>
+            </div>
+          </div>
+          <div className="home-header-content__right-content">
+            <a className="support" href="#">
+              <i class="fas fa-phone-square"></i>
+              <span>Hỗ trợ</span>
+            </a>
+            <a className="support" href="#">
+              024-7301-2468
+            </a>
+          </div>
         </div>
       </div>
     );
