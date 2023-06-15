02132c3fd3a6a56aaf8c4a60d2644228385f71dd
Thanh Bui <ltvphuoctien@gmail.com>
2023-06-15 21:56:18 +0700

    feat:format code

--- src/containers/App.js
+++ src/containers/App.js
@@ -50,19 +50,19 @@ class App extends Component {
             {this.props.isLoggedIn && <Header />}
 
             <div className="content-container">
-              <CustomScrollbars style={{height:'100vh', width:'100%'}}>
-              <Switch>
-                <Route path={path.HOME} exact component={Home} />
-                <Route
-                  path={path.LOGIN}
-                  component={userIsNotAuthenticated(Login)}
-                />
-                <Route
-                  path={path.SYSTEM}
-                  component={userIsAuthenticated(System)}
-                />
-                <Route path={path.HOMEPAGE} component={HomePage} />
-              </Switch>
+              <CustomScrollbars style={{ height: '100vh', width: '100%' }}>
+                <Switch>
+                  <Route path={path.HOME} exact component={Home} />
+                  <Route
+                    path={path.LOGIN}
+                    component={userIsNotAuthenticated(Login)}
+                  />
+                  <Route
+                    path={path.SYSTEM}
+                    component={userIsAuthenticated(System)}
+                  />
+                  <Route path={path.HOMEPAGE} component={HomePage} />
+                </Switch>
               </CustomScrollbars>
             </div>
 
